c21e2bd2a54dd193f8d2800f2ea235d6b4269aad
Nils Weiher <weiher@ub.uni-heidelberg.de>
2016-08-12 16:46:52 +0200

    Added method citeRenderAll to create bootstrap accordion to display multiple styles

--- citeproc-wrapper.js
+++ citeproc-wrapper.js
@@ -72,11 +72,83 @@
     return this;
   };
 
+  var createCitationPanel = function (bookId, style, opts) {
+     var panel = document.createElement('div');
+     panel.setAttribute('class', 'panel  panel-default');
+
+     var panel_heading = document.createElement('div');
+
+    var heading_id = "heading_" + style.id;
+    var headings = {"class":"panel-heading", "role":"tab", "id": heading_id};
+     $(panel_heading).attr(headings);
+
+     var panel_heading_title = document.createElement('h4');
+     panel_heading_title.setAttribute('class', 'panel-title');
+
+     var body_id = "body_" + style.id;
+     var heading_title_link_attributes = {"class":"collapsed", "role":"button", "data-toggle":"collapse","data-parent":"#" + opts.accordionID, "href": "#" + body_id ,"aria-controls": body_id, "aria-expanded": "false"};
+     var  heading_title = document.createElement('a');
+     heading_title.innerText = style.displayname;
+
+
+     $(heading_title).attr(heading_title_link_attributes);
+     panel_heading_title.appendChild(heading_title);
+     panel_heading.appendChild(panel_heading_title);
+     panel.appendChild(panel_heading);
+    var panel_body = document.createElement('div');
+
+     var body_attrs = {"class":"panel-collapse collapse", "role": "tabpanel", "id":body_id, "aria-labelledby": heading_id};
+     $(panel_body).attr(body_attrs);
+    var panel_body_div = document.createElement('div');
+    panel_body_div.setAttribute('class', 'panel-body');
+    panel_body.appendChild(panel_body_div);
+    // Attach event listener to panel body to display citation
+    $(panel_body).on('show.bs.collapse', function () {
+      $(panel_body_div).citeRender(bookId, {'style': style.id});
+      console.log("show " + style.id);
+    });
+
+    panel.appendChild(panel_body)
+    return panel;
+  };
+
+  $.fn.citeRenderAll = function (bookId, options) {
+    "use strict";
+    var opts = $.extend( {}, $.fn.citeRender.defaults, options );
+    var accordion = document.createElement('div');
+    $(accordion).attr({ "class": "panel-group", "id": opts.accordionId, "role":"tablist", "aria-multiselectable":"true"});
+    console.log("citeRenderAll");
+    console.log(opts);
+    opts.styles.forEach(function (style) {
+
+      var panel = createCitationPanel(bookId, style, opts);
+      console.log(panel);
+      accordion.appendChild(panel);
+    });
+    this.append(accordion);
+    return this;
+  };
+
+  // List of configured styles for the citation popup.
+  // id is the filename of the .csl file
+  var styles = [
+    {
+      "id": "chicago-fullnote-bibliography",
+      "displayname": "CMOS 16"
+    },
+    {
+      "id": "apa",
+      "displayname": "APA"
+    }
+  ];
+
   // Plugin defaults – added as a property on our plugin function.
   $.fn.citeRender.defaults = {
+    styles: styles,
     style: "chicago-fullnote-bibliography",
     lang: "de-DE",
     baseUrl: "/static/citeproc",
-    dataUrl: "/api/csl"
+    dataUrl: "/api/csl",
+    accordionID:"citationAccodion"
   };
 })( window, document, jQuery, CSL );
